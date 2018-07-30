3a815d543f7dd4f58cd903dd2ddafc84cb3ddd9b
oocjh2000 <oocjh200@gmail.com>
2018-07-30 10:46:58 +0900

    Add files via upload

---
+++ bt.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+  SerialUSB.begin(9600);
+  Serial5.begin(9600);
+
+  SerialUSB.println("aaaaa");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (SerialUSB.available()) {
+    Serial5.write(SerialUSB.read());
+  }
+  if (Serial5.available()) {
+    SerialUSB.write(Serial5.read());
+  }
+ //SerialUSB.println(analogRead(A0)) ;
+}
